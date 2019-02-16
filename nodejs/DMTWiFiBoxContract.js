cc289b6a45a3c0d4007c94670a555be48f36ee38
Eddie Lien <eddie.lien@gmail.com>
2019-02-16 11:35:27 +0800

    add http api for web and offline detection agent

--- nodejs/DMTWiFiBoxContract.js
+++ nodejs/DMTWiFiBoxContract.js
@@ -1,20 +1,25 @@
 const Web3 = require('web3')
+const fs = require('fs')
 const DMTWiFiBox = require('../build/contracts/DMTWiFiBox.json')
-
-// change to pi's wallet when pi's ready
-const truffleWallet = require('../truffle-config')
 
 // select network
 var args = process.argv.slice(2)
 const network = (args[0] !== '' ? args[0] : 'development')
 const netid = (network === 'development' ? 5777 : 238)
-const HTTP_PROVIDER = truffleWallet.networks[network].provider()
 const WS_PROVIDER = () => {
   return (netid === 5777)
     ? 'ws://127.0.0.1:8545'
     : 'wss://ws-proxy.dexon.org'
 }
 console.log('network', network, 'netid', netid)
+
+const writeFile = (path, data, opts = 'utf8') =>
+new Promise((resolve, reject) => {
+  fs.writeFile(path, data, opts, (err) => {
+    if (err) reject(err)
+    else resolve()
+  })
+})
 
 // class
 module.exports = {
@@ -31,55 +36,112 @@ module.exports = {
   // pi info
   pi: {},
 
+  // iplist
+  iplist: {},
+
+  // settings
+  settings: {},
+
+  init: function () {
+    this.settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'))
+  },
+
+  setup: async function (owner, privatekey) {
+    let ret = {status: false }
+    if (owner == '' || privatekey == '') {
+      ret.error = 'incorrect settings'
+      return ret
+    }
+    let r = await this.isReady()
+    if (!r.status) {
+      this.settings.owner = owner
+    }
+    if (this.settings.owner !== owner) {
+      ret = {status: false, error: 'not owner' }
+      return ret
+    }
+    this.settings.privatekey = privatekey
+    let json = JSON.stringify(this.settings)
+    try {
+      await writeFile('settings.json', json, 'utf8')
+    } catch (error) {
+      ret = {status: false, error: error}
+    }
+    return ret
+  },
+
+  isReady: async function () {
+    if (this.settings.privatekey === '') return {status: false, error: 'please setup this Pi first'}
+    let ret = {status: false}
+    if (this.contractWrite) {
+      return {status: true}
+    }
+    await this.getContractWrite().then(r => {
+      ret = r
+    })
+    return ret
+  },
+
+
   // get current pi wallet address
-  getAccounts: async function () {
+  getAccount: async function () {
     await this.walletHandler.eth.getAccounts().then(accounts => {
       if (accounts.length === 0) {
         console.log('error, no accounts')
-        return false
+        return {status: false, error: 'no accounts'}
       }
       this.walletHandler.eth.defaultAccount = accounts[0]
       this.pi.addr = accounts[0]
     })
-    return this.pi.addr
+    return {status: true, account: this.pi.addr}
   },
 
   // get contract write stream
   getContractWrite: async function () {
-    this.walletHandler = await new Web3(HTTP_PROVIDER)
-    this.contractWrite = await new this.walletHandler.eth.Contract(DMTWiFiBox.abi, DMTWiFiBox.networks[netid].address)
+    try {
+      let HTTP_PROVIDER = new HDWalletProvider(this.settings.privatekey, netid == 238 ? 'http://testnet.dexon.org:8545' : 'http://127.0.0.1:8545')
+      this.walletHandler = await new Web3(HTTP_PROVIDER)
+      this.contractWrite = await new this.walletHandler.eth.Contract(DMTWiFiBox.abi, DMTWiFiBox.networks[netid].address)  
+    } catch (error) {
+      return {status: false, error: 'incorrect settings', web3error: error}
+    }
+    return {status: true}
   },
 
   // get contract read stream and setup event listeners
   getContractRead: async function () {
-    this.handlerRead = new Web3(WS_PROVIDER())
-    this.contractRead = await new this.handlerRead.eth.Contract(DMTWiFiBox.abi, DMTWiFiBox.networks[netid].address)
-    this.contractRead.events.allEvents({}, function (error, event) {
-      if (error) {
-        console.log('event error', error, event)
-        return
-      }
-      switch (event.event) {
-        case 'online':
-          // event online(address indexed from, address indexed pi, uint ipaddr, uint maxtime);
-          // only handle events related with current Pi
-          if (event.returnValues.pi !== this.pi.addr) break
-          // execute shell to change iptables rules
-          console.log('online', event.returnValues)
-          break
-        case 'offline':
-          // event offline(address indexed from, address indexed owner, address pi, uint starttm, uint endtm, uint creditused);
-          // only handle events related with current Pi
-          if (event.returnValues.pi !== this.pi.addr) break
-          console.log('offline', event.returnValues)
-          // ignore this message in Pi
-          break
-        default:
-          console.log('event', event.event)
-      }
-    })
+    try {
+      this.handlerRead = new Web3(WS_PROVIDER())
+      this.contractRead = await new this.handlerRead.eth.Contract(DMTWiFiBox.abi, DMTWiFiBox.networks[netid].address)  
+      this.contractRead.events.allEvents({}, function (error, event) {
+        if (error) {
+          console.log('event error', error, event)
+          return
+        }
+        switch (event.event) {
+          case 'online':
+            // event online(address indexed from, address indexed pi, uint ipaddr, uint maxtime);
+            // only handle events related with current Pi
+            if (event.returnValues.pi !== this.pi.addr) break
+            // execute shell to change iptables rules
+            that.iplist[event.returnValues.ipaddr] = event.returnValues.from
+            console.log('online', event.returnValues)
+            break
+          case 'offline':
+            // event offline(address indexed from, address pi);
+            // only handle events related with current Pi
+            if (event.returnValues.pi !== this.pi.addr) break
+            console.log('offline', event.returnValues)
+            break
+          default:
+            console.log('event', event.event)
+        }      
+      })
+    } catch(error) {
+      return {status: false, error: error}
+    }
+    return {status: true}
   },
-
   // expose offline function to Pi
   offline: async function (useraddr) {
     try {
@@ -88,6 +150,8 @@ module.exports = {
       // error.reason now populated with an REVERT reason
       console.log('Failure reason', error.reason)
       console.log(error)
+      return {status: false, error: error}
     }
+    return {status: true}
   }
 }
---
+++ nodejs/IoTservice.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const wifiboxContract = require('./DMTWiFiBoxContract.js')
+
+wifiboxContract.init()
+
+async function startIoTservice () {
+  let ret = {}
+  await wifiboxContract.isReady().then(async function (r) {
+    ret = r 
+  })
+  if (!ret.status) return ret
+  
+  await wifiboxContract.getContractRead().then(r => {
+    ret = r
+    if (!r.status) console.log('contractRead ready')
+  })
+  if (!ret.status) return ret
+
+  await wifiboxContract.getAccount().then(r => {
+    ret = r
+    if (!r.status) console.log('use account', ret.account)
+  })
+  return ret
+}
+
+// start web service for offline detector
+const app = express()
+app.listen(3000)
+
+app.get('/', function (req, res) {
+  let ret = {status: true, title:"IoTservice", usage:
+    "/ipaddr get ip address\n"
+    + "/setup/:owneraddress/:privatekey setup with owner wallet address and private key for this Pi\n"
+    + "/offline/:ipaddr for offline detection agent to nitify IoTservice that user on this ip address is offline now\n"
+  }
+  res.send(JSON.stringify(ret))
+})
+
+app.get('/ipaddr', function(req, res) {
+  let ret = {status: true, ipaddr: req.connection.remoteAddress}
+  res.send(JSON.stringify(ret))
+})
+
+app.get('/setup/:owneraddress/:privatekey', async function(req, res) {
+  let piwallet = false
+  await wifiboxContract.setup(req.params.owneraddress, req.params.privatekey).then(r => {
+    console.log('setup', r)
+    piwallet = r
+  })
+  let ret = {status: false}
+  if (piwallet) {
+    await startIoTservice().then(r => {
+      ret = r
+    })
+  }
+  res.send(JSON.stringify(ret))
+})
+
+app.get('/offline/:ipaddr', async function(req, res) {
+  let ret = {status: false}
+  if (req.connection.remoteAddress !== '127.0.0.1' && req.connection.remoteAddress !== '::ffff:127.0.0.1') {
+      ret.error = 'localhost only, your ip is ' + req.connection.remoteAddress 
+    res.send(JSON.stringify(ret))
+    return
+  }
+  await wifiboxContract.isReady().then(r => {
+    ret = r
+  })
+  if (ret.status) {
+    await wifiboxContract.offline(req.params.ip).then(r => {
+      ret = r
+    })
+  }
+  res.send(JSON.stringify(ret))
+})
+
